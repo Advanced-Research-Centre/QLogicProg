24d29913cb096deaa2f771689f1c110da62def2a
Aqasch <58222498+Aqasch@users.noreply.github.com>
2023-02-24 16:41:48 +0100

    All possible classical query added

--- src/qlexer.py
+++ src/qlexer.py
@@ -2,22 +2,25 @@ from sly import Lexer
 
 class QLexer(Lexer):
 
-    tokens = {PROG, NOUN, ADJ, COUNTS, VERB, COUNTOF, ADJOF, QUERY}
+    tokens = {PROG, NOUN, ADJ, COUNTS, VERB, 
+              COUNTOF, ADJOF, QUERY, FORALL, ADD}
     
     ignore = ' \t\n'
 
-    PROG        = r'p_[a-zA-Z_][a-zA-Z0-9_]*' # TODO: Extract only things after underscore : DONE!
-    NOUN        = r'n_[a-zA-Z_][a-zA-Z0-9_]*'
-    ADJ         = r'a_[a-zA-Z_][a-zA-Z0-9_]*'
-    VERB        = r'v_[a-zA-Z_][a-zA-Z0-9_]*'
+    PROG        = r'p_([a-zA-Z_][a-zA-Z0-9_]*)' # TODO: Extract only things after underscore : DONE!
+    NOUN        = r'n_([a-zA-Z_][a-zA-Z0-9_]*)'
+    ADJ         = r'a_([a-zA-Z_][a-zA-Z0-9_]*)'
+    VERB        = r'v_([a-zA-Z_][a-zA-Z0-9_]*)'
     COUNTOF     = r'::'
     ADJOF       = r'<-'
     QUERY       = r'\?'
     COUNTS      = r'\d+'
+    FORALL      = r'\*'
+    ADD         = r'\+'
 
 # if __name__ == '__main__':
-#     data = '3::a_red <- n_ball'
-#     lexer = MyLexer()
+#     data = '?::* <- n_ball'
+#     lexer = QLexer()
 #     for tok in lexer.tokenize(data):
 #         if tok.type in ['PROG', 'NOUN', 'ADJ', 'VERB']:
 #             tok.value = tok.value[tok.value.index('_')+1:]
--- src/qparser.py
+++ src/qparser.py
@@ -2,7 +2,7 @@ from sly import Parser
 from qlexer import QLexer
 
 class QParser(Parser):
-    # Get the token list from the lexer (required)
+    # Get the token list from the lexer
     tokens = QLexer.tokens
 
     d = dict()
@@ -24,7 +24,39 @@ class QParser(Parser):
 
     @_('QUERY COUNTOF ADJ ADJOF NOUN')
     def expr(self, p):
-        print(self.d[p[4]][p[2]]) 
+        print(f'Count of {p[2]} :', self.d[p[4]][p[2]])
+    
+    @_('QUERY COUNTOF FORALL ADJOF NOUN')
+    def expr(self, p):
+        tot_count = 0
+        for k in self.d[p[4]].keys():
+            print(k, self.d[p[4]][k])
+            tot_count += int(self.d[p[4]][k])
+        print(tot_count)
+
+    @_('QUERY COUNTOF FORALL ADJOF FORALL')
+    def expr(self, p):
+        tot_count = 0
+        for k1 in self.d.keys():
+            semi_tot_count = 0
+            for k2 in self.d[k1].keys():
+                semi_tot_count += int(self.d[k1][k2])
+            tot_count += semi_tot_count
+        tot_count = str(tot_count)
+        print(f'total Count for all Adj and Noun', tot_count)
+
+    @_('FORALL COUNTOF QUERY ADJOF NOUN')
+    def expr(self, p):
+        list_adj = []
+        for k in self.d[p[4]].keys():
+            list_adj.append(k)
+        print(list_adj)
+    
+    @_('ADD COUNTS COUNTOF ADJ ADJOF NOUN')
+    def expr(self, p):
+        self.d[p[5]][p[3]] = str(int(self.d[p[5]][p[3]])+int(p[1]))
+        print(self.d)
+
 
 if __name__ == '__main__':
     lexer = QLexer()
