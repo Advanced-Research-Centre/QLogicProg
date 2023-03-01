a72922191a47c4db6ddb9ee40c0fa3ef85274031
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2023-03-01 11:03:37 +0100

    cleanup and new prog for boolean algebra

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # QLogicProg
 
+## Preliminary documentation
+
+### Project motivation
+[Automated Quantum Software Engineering: why? what? how?](https://arxiv.org/abs/2212.00619)
+
 ### How to use
-1. pip install sly
-2. >> python qlp_compiler.py qprog.qlp
+1. >> pip install sly
+2. >> python qlp_compiler.py examples\qprog1.qlp
+
+### Language design
+QLP is inspired by ProbLog2 (inturn from ProbLog, DataLog and ProLog), qThought, Lolli and SilQ.
+
+## For developers
 
 ### Sly links
 1. https://yahel-oppenheimer.medium.com/create-your-own-scripting-language-in-python-with-sly-7b864e762e07
 2. https://replit.com/talk/learn/Making-your-own-programming-language-with-Python/45767
 3. https://www.geeksforgeeks.org/how-to-create-a-programming-language-using-python/
 
-### Project motivation
-[Automated Quantum Software Engineering: why? what? how?](https://arxiv.org/abs/2212.00619)
--- src/qparser.py
+++ src/qparser.py
@@ -28,7 +28,10 @@ class QParser(Parser):
     
     @_('QUERY COUNTOF ADJ ADJOF NOUN')
     def expr(self, p):
-        print('Total Counts of',p[4][2:],'for',p[2][2:],':', self.states[p[4][2:]][p[2][2:]])
+        if p[2][2:] in self.states[p[4][2:]].keys():
+            print('Total Counts of',p[4][2:],'for',p[2][2:],':', self.states[p[4][2:]][p[2][2:]])
+        else:
+            print('Total Counts of',p[4][2:],'for',p[2][2:],': Null')
     
     @_('QUERY COUNTOF FORALL ADJOF NOUN')
     def expr(self, p):
@@ -58,53 +61,27 @@ class QParser(Parser):
     @_('VERB NOUNTGT DEFVERB VERBDEF')
     def expr(self, p):
         if p[0][2:] not in self.processes:
-            self.processes[p[0][2:]] = [int(p[1][1:-1]), {}]#{}
-            # print(self.processes)
+            self.processes[p[0][2:]] = [int(p[1][1:-1]), {}]
             v_def = p[3][1:-1]
-
             lr = re.compile(r'([\S]*):([\S]*)')
             parts = lr.search(v_def).groups()
-            # print(parts)
             tgt = re.compile(r'[0-9+-]*a_([a-zA-Z][a-zA-Z0-9_]*)\|([0-9]*)\|[0-9+-]*')
             parts2 = tgt.search(parts[0]).groups()
             # print(parts2,parts[1])
-            # print(self.processes[p[0][2:]][1])
             self.processes[p[0][2:]][1][parts2[0]] = [int(parts2[1]),parts[1]]
-            print(parts)
-            # exit()
-            # indx = v_def.index(':')
-            # v_def_left, v_def_right = v_def[ : indx], v_def[ indx+1:]
-            # print(v_def_left, v_def_right)
-            # for case_no, left_right_case in enumerate([v_def_left, v_def_right]):
-            #     indices_pipe = [i for i, x in enumerate(left_right_case) if x == '|']
-            #     targ_noun = int(v_def_left[ int(indices_pipe[0])+1: int(indices_pipe[1]) ])
-            #     process_dict[p[0][2:]][int(p[1][1:-1])][targ_noun] = 0
-            #     if case_no != 0:
-            #         operation = left_right_case[indices_pipe[-1]+1:indices_pipe[-1]+2]
-            #         indx_operation = left_right_case.index(operation)
-            #         out_after_v = left_right_case[indx_operation+1:]   
-            # print(process_dict)    
-            # exit()
-            
-            # self.processes[p[0][2:]] = [int(p[1][1:-1]),p[3][1:-1]]
-
         # print('Processes Dictionary :',self.processes)
 
     @_('VERB VERBON NOUN')
     def expr(self, p):
-        dig = self.processes[ p[0][2:] ][1]
+        print('Applying',p[0][2:],'on',p[2][2:])
+        src_verb = self.processes[p[0][2:]][1]
         baler_dict = self.states[p[2][2:]]
-        arekta_baler_dict = self.processes[ p[0][2:] ]
-        for k in dig.keys():
+        # print(src_verb)
+        for k in src_verb.keys():
             if k in baler_dict.keys():
-                baler_string = arekta_baler_dict[1][k][1]
+                baler_string = src_verb[k][1]
                 tgt = re.compile(r'[0-9+-]*a_([a-zA-Z][a-zA-Z0-9_]*)\|([0-9]*)\|[0-9+-]*')
                 parts = tgt.search(baler_string).groups()
                 baler_string = baler_string.replace('a_'+parts[0]+'|'+parts[1]+'|', str(baler_dict[k]))
                 baler_dict[k] = eval(baler_string)
     
-    '''
-    @_('ADD COUNTS COUNTOF ADJ ADJOF NOUN')
-    def expr(self, p):
-        self.states[p[5][2:]][p[3][2:]] = self.states[p[5][2:]][p[3][2:]]+int(p[1])
-    '''
