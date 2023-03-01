ed393e0d5fce43f0091c0bf0d2a66371929b2106
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2023-03-01 12:22:19 +0100

    verb with one noun multiple adj done

--- src/qparser.py
+++ src/qparser.py
@@ -1,6 +1,8 @@
 from sly import Parser
 from src.qlexer import QLexer
 import re
+import copy
+import json
 
 class QParser(Parser):
     
@@ -63,17 +65,35 @@ class QParser(Parser):
         if p[0][2:] not in self.processes:
             self.processes[p[0][2:]] = [int(p[1][1:-1]), {}]
             v_def = p[3][1:-1]
-            lr = re.compile(r'([\S]*):([\S]*)')
-            parts = lr.search(v_def).groups()
-            tgt = re.compile(r'[0-9+-]*a_([a-zA-Z][a-zA-Z0-9_]*)\|([0-9]*)\|[0-9+-]*')
-            parts2 = tgt.search(parts[0]).groups()
-            # print(parts2,parts[1])
-            self.processes[p[0][2:]][1][parts2[0]] = [int(parts2[1]),parts[1]]
-        # print('Processes Dictionary :',self.processes)
-
+            v_ops = v_def.split(",")
+            for op in v_ops:
+                split_left_right = re.compile(r'([\S]*):([\S]*)')
+                op_assign = split_left_right.search(op).groups()
+                self.processes[p[0][2:]][1][op_assign[0]] = op_assign[1]
+    
+    # One noun multiple adj
     @_('VERB VERBON NOUN')
     def expr(self, p):
         print('Applying',p[0][2:],'on',p[2][2:])
+        #print('[DEBUG] Processes Dictionary :\n',json.dumps(self.processes, indent=2, default=str))
+        src_verb = copy.deepcopy(self.processes[p[0][2:]][1])
+        copy_noun = copy.deepcopy(self.states[p[2][2:]])
+        baler_dict = self.states[p[2][2:]]
+        for k in src_verb.keys():
+            for adj in copy_noun.keys():
+                src_verb[k] = src_verb[k].replace('a_'+adj+'|1|', str(copy_noun[adj])) 
+        for k in src_verb.keys():
+            src_verb[k] = eval(src_verb[k])
+        for k in src_verb.keys():
+            self.states[p[2][2:]][k[2:-3]] = src_verb[k]                
+
+    # One noun one adj
+    """
+    @_('VERB VERBON NOUN')
+    def expr(self, p):
+        print('Applying',p[0][2:],'on',p[2][2:])
+        print('[DEBUG] Processes Dictionary :')#,self.processes)
+        print (json.dumps(self.processes, indent=2, default=str))
         src_verb = self.processes[p[0][2:]][1]
         baler_dict = self.states[p[2][2:]]
         # print(src_verb)
@@ -84,4 +104,4 @@ class QParser(Parser):
                 parts = tgt.search(baler_string).groups()
                 baler_string = baler_string.replace('a_'+parts[0]+'|'+parts[1]+'|', str(baler_dict[k]))
                 baler_dict[k] = eval(baler_string)
-    
+    """
